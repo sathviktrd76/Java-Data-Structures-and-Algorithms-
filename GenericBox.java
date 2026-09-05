0b3d5577f5fb065802c1fbf1025d70d8ab17eb0d
Sathvik Talabathula <mail2sathvik67@gmail.com>
2026-09-05 12:04:39 +0530

    1. Generic Box

---
+++ GenericBox.java
@@ -0,0 +1,24 @@
+class Box<T>{
+
+    private T value;
+
+    public void setValue(T value){
+        this.value=value;
+    }
+
+    T getValue(){
+        return value;
+    }
+}
+
+public class GenericBox{
+    public static void main(String[] args){
+        Box<Integer> intBox=new Box<>();
+        intBox.setValue(76);
+        System.out.println("Integer : "+intBox.getValue());
+
+        Box<String> strBox=new Box<>();
+        strBox.setValue("Sathvik");
+        System.out.println("String : "+strBox.getValue());
+    }
+}
