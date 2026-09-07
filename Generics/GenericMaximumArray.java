2a051bd76306d220bb473a9650733bdd6468cfda
Sathvik Talabathula <mail2sathvik67@gmail.com>
2026-09-07 20:59:01 +0530

    Add generic maximum for array

---
+++ Generics/GenericMaximumArray.java
@@ -0,0 +1,18 @@
+public class GenericMaximumArray{
+    public static <T extends Comparable<T>> T maximum(T[] arr){
+        T max=arr[0];
+
+        for(int i=1;i<arr.length;i++){
+            if(arr[i].compareTo(max)>0){
+                max=arr[i];
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args){
+        Integer numbers[]={10,20,30,40,50};
+        Integer result=maximum(numbers);
+        System.out.println("Maximum : "+result);
+    }
+}
